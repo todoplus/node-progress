eab528694a5f057ac3c58885055f35a473a198c8
adrianiten <user1@user1s-MBP.fritz.box>
2014-10-30 17:36:47 +0100

    someprogress

--- Parseserver edit.js
+++ Parseserver edit.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://test:test@proximus.modulusmongo.net:27017/tap4eXub');
+var Brot = require('./model/Brotschema.js');
 
-app.get('/endpoint', function(request, response) {
-    var id = request.query.id;
-    response.end("I have received the ID: " + id);
-    console.log(id);
+app.get('/put', function(request, response) {
+    var usr = request.query.usr;
+    var text = request.query.text;
+    response.end("I have received: " + usr + text);
+    var b = new Brot({ name: text, user: usr});
+    b.save();
+    console.log(b);
+    console.log("Added " +text +usr);
+
 });
 
 app.listen(8080);
--- putBrot.js
+++ putBrot.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://test:test@proximus.modulusmongo.net:27017/tap4eXub');
 var Brot = require('./model/Brotschema.js');
-var b = new Brot({ name: "testaggaas", user: "testuser2"}); // effektivs Todo, custom user und logischerwiis Todo, Datum das vu jez
+var b = new Brot({ name: "testaggaas", user: "testuser2"}); // effektivs Todo, user
 b.save();
 console.log(b);
