2baf8d7b390f23921bc4378b654f91656aabe285
adrianiten <user1@user1s-MBP.fritz.box>
2014-11-02 22:43:22 +0100

    del

---
+++ Server_put_get_deletebyid.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://test:test@proximus.modulusmongo.net:27017/tap4eXub');
+var Brot = require('./model/Brotschema.js');
+
+//put (m usr und text)
+app.get('/put', function(request, response) {
+    var usr = request.query.usr;
+    var text = request.query.text;
+    var b = new Brot({ name: text, user: usr});
+    b.save();
+    console.log("Added " +b);
+    response.end("I have added the following: " +b);
+});
+
+//get (nach usr)
+app.get('/get', function(request, response) {
+    var usr = request.query.usr;
+    Brot.find({user: usr}, function (err, Brots) {
+       console.log("Got all the following Todos for the user " +usr);
+       console.log(Brots);
+       response.json(Brots);
+    });
+});
+
+//delete (nach id)
+app.get('/rmv', function(request, response) {
+    var id = request.query.id;
+    Brot.remove({_id:id}, function(err){
+       if(err) throw err;
+       console.log("Removed the Todo with the id " + id);
+       response.end("Removed the Todo with the id " + id);
+    });
+});
+
+app.listen(8080);
+console.log("node express app started at http://localhost:8080");
--- removethisshit neu.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://test:test@proximus.modulusmongo.net:27017/tap4eXub');
-var Todo = require('/Users/user1/Documents/new/model/Todoschemaneu.js');
-Todo.findOne(function(err, Todo){
-Todo.remove()
-})
