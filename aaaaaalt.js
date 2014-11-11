9e1421d65ea874412f8ae57299b21a5dd1f9aa11
adrianiten <user1@user1s-MBP.fritz.box>
2014-11-11 18:35:42 +0100

    newtry

--- aaaaaalt.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var List = require('/Users/user1/Documents/new/model/ListSchema.js');
-var c = new List({content: "Todo", priority: "lol", date: "16.10.14", device: "MBookPro", account: "Testaccount"});
-c.save()
-console.log(c)
-mongoose.connect('mongodb://test:test@proximus.modulusmongo.net:27017/tap4eXub');
