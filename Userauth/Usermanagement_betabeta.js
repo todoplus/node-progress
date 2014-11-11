f39a4d1e8fe9878dc2d570c222338c6760bb2c4a
adrianiten <user1@user1s-MBP.fritz.box>
2014-11-11 19:02:46 +0100

    fancy

--- Userauth/Usermanagement_betabeta.js
+++ Userauth/Usermanagement_betabeta.js
@@ -19,13 +19,14 @@ app.get('/get', function(request, response) {
        console.log("Get angefordert für den User " +usr);
        if (c < 1) {
           console.log("User not found");
+          console.log("");
        }
        
        if (c == 1) {
           console.log("Login ok");
           Todo.find({user: usr}, function (err, Todos) {
-             console.log("Got all the following Todos for the user " +usr);
-             console.log(Todos);
+             console.log("Got all the Todos for the user " +usr);
+             console.log("");
              response.json(Todos);
           });
        }
@@ -45,11 +46,13 @@ app.get('/createuser', function(request, response) {
           var b = new User({ username: usr, pass: thepass});
           b.save();
           console.log("Added the user: " +b);
+          console.log("");
           response.end("I have added the following: " +b)
        }
        else {
           response.end("Your username is already taken!");
           console.log("This username is already taken: " +usr);
+          console.log("");
        }
     });
 });
@@ -62,8 +65,8 @@ app.get('/put', function(request, response) {
     var number = User.count({username: usr, pass: thepass}, function(err, c) {
        console.log("Put angefordert für den User " +usr);
        if (c < 1) {
-          console.log("User not found")
-          
+          console.log("User not found");
+          console.log("");
        }
        
        if (c == 1) {
@@ -71,6 +74,7 @@ app.get('/put', function(request, response) {
           var b = new Todo({name: text, user: usr});
           b.save();
           console.log("Added" + b);
+          console.log("");
           response.json(b);
        }
        
@@ -80,6 +84,34 @@ app.get('/put', function(request, response) {
     });
 });
 
+//remove
+app.get('/rmv', function(request, response) {
+    var id = request.query.id;
+    var usr = request.query.usr;
+    var thepass = request.query.pass;
+    var number = User.count({username: usr, pass: thepass}, function(err, c) {
+       console.log("Rmv angefordert für den User " +usr);
+       if (c < 1) {
+          console.log("User not found");
+          console.log("");   
+       }
+       
+       if (c == 1) {
+          console.log("Login ok");
+          Todo.remove({_id:id}, function(err) {
+             console.log("Removed the Todo with the id " + id);
+             console.log("");
+             response.end("Removed the Todo with the id " + id);
+          });
+       }
+       
+       else {
+          console.log("Unexpected error, more than one user with your username")
+       }
+    });
+});
 
+
+//run
 app.listen(8080);
-console.log("node express app started at http://localhost:8080");
+console.log("Running on Port 8080");
