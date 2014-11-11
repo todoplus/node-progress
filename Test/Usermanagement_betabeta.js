75b23fe86f661a2084862f5d3077e4160455d5a0
adrianiten <user1@user1s-MBP.fritz.box>
2014-11-11 16:03:31 +0100

    Userlogik passt

--- Test/Usermanagement_betabeta.js
+++ Test/Usermanagement_betabeta.js
@@ -8,6 +8,9 @@ var User = require('./model/Userschema.js');
 //get (nach usr)
 app.get('/get', function(request, response) {
     var usr = request.query.usr;
+    var number = User.count({username: usr}, function(err, c) {
+       console.log(c)
+       })
     User.find({username: usr}, function (err, Users) {
        console.log(Users);
        response.json(Users);
@@ -18,10 +21,20 @@ app.get('/get', function(request, response) {
 app.get('/put', function(request, response) {
     var usrname = request.query.usr;
     var thepass = crypto.createHash('md5').update(request.query.pass).digest('hex');
-    var b = new User({ username: usrname, pass: thepass});
-    b.save();
-    console.log("Added " +b);
-    response.end("I have added the following: " +b);
+    var number = User.count({username: usrname}, function(err, c) {
+       console.log(c)
+       if (c < 1) {
+          var b = new User({ username: usrname, pass: thepass});
+          b.save();
+          console.log("Added the user: " +b);
+          response.end("I have added the following: " +b)
+       }
+       else {
+          response.end("Your username is already taken!");
+          console.log("This username is already taken: " +usrname);
+       }
+    });
+   
 });
 
 app.listen(8080);
