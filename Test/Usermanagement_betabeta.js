a4ed9614ed0c0506fb2068c2b772dcd776432e1d
adrianiten <user1@user1s-MBP.fritz.box>
2014-11-11 18:12:27 +0100

    put still not really secure

--- Test/Usermanagement_betabeta.js
+++ Test/Usermanagement_betabeta.js
@@ -13,18 +13,18 @@ var Todo = require('./model/Todoschema.js');
 
 //get (nach usr)
 app.get('/get', function(request, response) {
-    var usrname = request.query.usrname;
-    var thepass = request.query.thepass;
-    var number = User.count({username: usrname, pass: thepass}, function(err, c) {
-       console.log("Get angefordert für den User " +usrname);
+    var usr = request.query.usr;
+    var thepass = request.query.pass;
+    var number = User.count({username: usr, pass: thepass}, function(err, c) {
+       console.log("Get angefordert für den User " +usr);
        if (c < 1) {
           console.log("User not found")
        }
        
        if (c = 1) {
           console.log("Login ok");
-          Todo.find({user: usrname}, function (err, Todos) {
-             console.log("Got all the following Todos for the user " +usrname);
+          Todo.find({user: usr}, function (err, Todos) {
+             console.log("Got all the following Todos for the user " +usr);
              console.log(Todos);
              response.json(Todos);
           });
@@ -32,55 +32,52 @@ app.get('/get', function(request, response) {
        else {
           console.log("Unexpected error, more than one user with your username");
        }
+    });
 });
 
 // Registrierung mit parameter in klartext, der dann in md5 verschlüsselt wird
 // Nennemers verschlüssle :D
 app.get('/createuser', function(request, response) {
-    var usrname = request.query.usr;
+    var usr = request.query.usr;
     var thepass = crypto.createHash('md5').update(request.query.pass).digest('hex');
-    var number = User.count({username: usrname}, function(err, c) {
-       console.log(c)
+    var number = User.count({username: usr}, function(err, c) {
        if (c < 1) {
-          var b = new User({ username: usrname, pass: thepass});
+          var b = new User({ username: usr, pass: thepass});
           b.save();
           console.log("Added the user: " +b);
           response.end("I have added the following: " +b)
        }
        else {
           response.end("Your username is already taken!");
-          console.log("This username is already taken: " +usrname);
+          console.log("This username is already taken: " +usr);
        }
     });
 });
 
 //Put - und ja das wird kritisch
 app.get('/put', function(request, response) {
-    var usrname = request.query.usrname;
-    var thepass = request.query.thepass;
+    var usr = request.query.usr;
+    var thepass = request.query.pass;
     var text = request.query.text;
-    var number = User.count({username: usrname, pass: thepass}, function(err, c) {
-       console.log("Get angefordert für den User " +usrname);
+    var number = User.count({username: usr, pass: thepass}, function(err, c) {
+       console.log("Put angefordert für den User " +usr);
        if (c < 1) {
           console.log("User not found")
        }
        
        if (c = 1) {
           console.log("Login ok");
-          var b = new todo({name: text, user: usrname});
+          var b = new Todo({name: text, user: usr});
           b.save();
           console.log("Added" + b);
           response.json(b);
        }
        
        else {
-          console.log("Unexpected error, more than one user with your username");
+          console.log("Unexpected error, more than one user with your username")
        }
     });
 });
-
-
-
 
 
 app.listen(8080);
