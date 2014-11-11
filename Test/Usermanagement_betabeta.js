3cc8d3c73e6fd5b2562d225fff5d20ba58a3d29f
adrianiten <user1@user1s-MBP.fritz.box>
2014-11-11 17:44:34 +0100

    errorsearchongoing

--- Test/Usermanagement_betabeta.js
+++ Test/Usermanagement_betabeta.js
@@ -1,24 +1,42 @@
+//Module
 var express = require('express');
 var crypto = require('crypto');
 var app = express();
 var mongoose = require('mongoose');
+
+//DB-Connection
 mongoose.connect('mongodb://test:test@proximus.modulusmongo.net:27017/tap4eXub');
+
+//MongoDB-Schemas
 var User = require('./model/Userschema.js');
+var Todo = require('./model/Todoschema.js');
 
 //get (nach usr)
 app.get('/get', function(request, response) {
-    var usr = request.query.usr;
-    var number = User.count({username: usr}, function(err, c) {
-       console.log(c)
-       })
-    User.find({username: usr}, function (err, Users) {
-       console.log(Users);
-       response.json(Users);
-    });
+    var usrname = request.query.usrname;
+    var thepass = request.query.thepass;
+    var number = User.count({username: usrname, pass: thepass}, function(err, c) {
+       console.log("Get angefordert für den User " +usrname);
+       if (c < 1) {
+          console.log("User not found")
+       }
+       
+       if (c = 1) {
+          console.log("Login ok");
+          Todo.find({user: usrname}, function (err, Todos) {
+             console.log("Got all the following Todos for the user " +usrname);
+             console.log(Todos);
+             response.json(Todos);
+          });
+       }
+       else {
+          console.log("Unexpected error, more than one user with your username");
+       }
 });
 
-//put (m usr und text)
-app.get('/put', function(request, response) {
+// Registrierung mit parameter in klartext, der dann in md5 verschlüsselt wird
+// Nennemers verschlüssle :D
+app.get('/createuser', function(request, response) {
     var usrname = request.query.usr;
     var thepass = crypto.createHash('md5').update(request.query.pass).digest('hex');
     var number = User.count({username: usrname}, function(err, c) {
@@ -34,8 +52,36 @@ app.get('/put', function(request, response) {
           console.log("This username is already taken: " +usrname);
        }
     });
-   
 });
+
+//Put - und ja das wird kritisch
+app.get('/put', function(request, response) {
+    var usrname = request.query.usrname;
+    var thepass = request.query.thepass;
+    var text = request.query.text;
+    var number = User.count({username: usrname, pass: thepass}, function(err, c) {
+       console.log("Get angefordert für den User " +usrname);
+       if (c < 1) {
+          console.log("User not found")
+       }
+       
+       if (c = 1) {
+          console.log("Login ok");
+          var b = new todo({name: text, user: usrname});
+          b.save();
+          console.log("Added" + b);
+          response.json(b);
+       }
+       
+       else {
+          console.log("Unexpected error, more than one user with your username");
+       }
+    });
+});
+
+
+
+
 
 app.listen(8080);
 console.log("node express app started at http://localhost:8080");
---
+++ Test/model/Todoschema.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var TodoSchema = new mongoose.Schema({
+    name: {type: String, default: ""},
+    user: {type: String, default: ""},
+    Date: {type: Date, default: Date.now}
+});
+module.exports = mongoose.model('Todo', TodoSchema);
