93ca23405455fd4758c9d72ae4e879b38fcbffbf
adrianiten <user1@user1s-MBP.fritz.box>
2014-12-21 17:07:45 +0100

    ssid_done?

--- RESTful/restapi_flissigwieimmer.js
+++ RESTful/restapi_flissigwieimmer.js
@@ -65,59 +65,69 @@ app.post('/api/login', function(req, res) {
    var device1 = req.body.device;
    var thepass1 = req.body.pass;
 //Abgleich mit Login-Daten und c gleich wieviele User mit dieser Kombination
-   User.findOne({username:usr}, function(err, e) {       
-      var salter1 = e.salter;
-      var thepass = crypto.createHash('sha512').update(thepass1+salter1).digest('hex');
-      var number = User.count({username: usr, pass: thepass}, function(err, c) {
-         console.log("Loginversuch für den Usernamen " +usr);
-         if (c == 1) {
-            console.log(stat000);
-            console.log("");
-            SSID.count({username: usr, device: device1}, function (err, z) {
-               if (z == 1) {
-                  SSID.findOne({username: usr, device: device1}, function (err, f) {
-                     res.json(Array({"user":usr, "ssid":f.ssid}));
-                     res.end();
+   User.count({username:usr}, function(err, y) {  
+      if (y == 1) { 
+         User.findOne({username:usr}, function(err, e) {  
+            var salter1 = e.salter;
+            var thepass = crypto.createHash('sha512').update(thepass1+salter1).digest('hex');
+            var number = User.count({username: usr, pass: thepass}, function(err, c) {
+               console.log("Loginversuch für den Usernamen " +usr);
+               if (c == 1) {
+                  console.log(stat000);
+                  console.log("");
+                  SSID.count({username: usr, device: device1}, function (err, z) {
+                     if (z == 1) {
+                        SSID.findOne({username: usr, device: device1}, function (err, f) {
+                           res.json(Array({"user":usr, "ssid":f.ssid}));
+                           res.end();
+                        });
+                     }
+                     else {
+                        var salter2 = rs.generate();
+                        var thepass2 = crypto.createHash('sha512').update(device1+salter2).digest('hex');
+                        var g = new SSID({username: usr, salter: salter2, ssid: thepass2, device: device1});
+                        g.save();
+                        res.json(Array({"user":usr, "ssid":g.ssid}));
+                        res.end();
+                     }
                   });
                }
+
                else {
-                  var salter2 = rs.generate();
-                  var thepass2 = crypto.createHash('sha512').update(device1+salter2).digest('hex');
-                  var g = new SSID({username: usr, salter: salter2, ssid: thepass2, device: device1});
-                  g.save();
-                  res.json(Array({"user":usr, "ssid":g.ssid}));
+                  console.log(stat001);
+                  console.log("");
+                  res.json(stat001);
                   res.end();
                }
             });
-         }
+        });
+      }
+      else {
+         console.log(stat001);
+         res.json(stat001);
+         res.end();
+      }
 
-         else {
-            console.log(stat001);
-            console.log("");
-            res.json(stat001);
-            res.end();
-         }
-      });
    });
 });
 
 //POST
 app.post('/api', function(req, res) {
-    var usr = req.body.usr;
     var ssid = req.body.ssid;
     var text = req.body.text;
-    var device = req.body.device;
     var shared = req.body.shared;
-    var number = SSID.count({username: usr, ssid: ssid, device:device}, function(err, c) {
-       console.log("POST angefordert für den User " +usr);
+    var number = SSID.count({ssid: ssid}, function(err, c) {
        if (c == 1) {
-          console.log(stat000);
-          var b = new Todo({name: text, user: usr, sharedw: shared});
-          b.save();
-          console.log("Added" + b);
-          console.log("");
-          res.json(Array(b));
-          res.end();
+          SSID.findOne({ssid:ssid}, function(err, e) {  
+             console.log("Der User " +e.username+ " hat POST angefordert");
+             console.log(stat000);
+             var b = new Todo({name: text, user: e.username, sharedw: shared});
+             b.save();
+             console.log("Added" + b);
+             console.log("");
+             res.json(Array(b));
+             res.end();
+          });  
        }
        
        else {
@@ -132,38 +142,37 @@ app.post('/api', function(req, res) {
 //put
 app.put('/api/:Todo_id', function(req, res) {
 //authent und text in body, id als param
-    var usr = req.body.usr;
     var ssid = req.body.ssid;
-    var device = req.body.device;
     var updatedtext = req.body.text;
 //    var shared = req.body.shared;
     var id = req.params.Todo_id;
 //login-überprüfung
-    var number = SSID.count({username: usr, ssid: ssid, device: device}, function(err, c) {
-       console.log("PUT angefordert für den User " +usr);
+    var number = SSID.count({ssid:ssid}, function(err, c) {
        if (c == 1) {
 //update, wenn login erfolgreich
-          console.log(stat000);
-          Todo.count({ user: usr, _id:id }, function (err, doc) {
+          SSID.findOne({ssid:ssid}, function(err, e) {  
+             console.log(stat000);
+             console.log("Der User" +e.username+ " hat PUT angefordert");
+             Todo.count({ user: e.username, _id:id }, function (err, doc) {
              if (doc == 1) {
-                Todo.findOne({ user: usr, _id:id }, function (err, td) {
+                Todo.findOne({ user: e.username, _id:id }, function (err, td) {
                    td.name = updatedtext;
                    td.save();
                    res.json(Array(td));
                    res.end();
-                   console.log("Updated a Todo for the user " +usr); 
+                   console.log("Updated a Todo for the user " +e.username); 
                    console.log("");  
                 });
              }
              else {
-                var number2 = Todo.count({sharedw: usr, _id: id}, function(err, f) {
+                var number2 = Todo.count({sharedw: e.username, _id: id}, function(err, f) {
                    if (f == 1) {
-                      Todo.findOne({ sharedw: usr, _id:id }, function (err, bli){
-                         bli.name = updatedtext;
-                         bli.save();
-                         res.json(Array(bli));
+                      Todo.findOne({ sharedw: e.username, _id:id }, function (err, j){
+                         j.name = updatedtext;
+                         j.save();
+                         res.json(Array(j));
                          res.end();
-                         console.log("Updated a shared Todo for the user " +usr); 
+                         console.log("Updated a shared Todo for the user " +e.username); 
                          console.log("");  
                       });
       
@@ -177,6 +186,7 @@ app.put('/api/:Todo_id', function(req, res) {
 
                });
              }
+           });
         });
        }
        else {
@@ -189,49 +199,45 @@ app.put('/api/:Todo_id', function(req, res) {
 
 
 //delete
-app.delete('/api/:Todo_id/:User_name/:User_ssid/:User_device', function(req, res) {
-//authent und in body, id als param
-//body gad schinbar nöd
-//    var usr = req.body.usr;
-//    var thepass = req.body.pass;
+app.delete('/api/:Todo_id/:User_ssid/', function(req, res) {
    var id = req.params.Todo_id;
-   var usr = req.params.User_name;
    var ssid = req.params.User_ssid;
-   var device = req.params.User_device
 //login-überprüfung
-    var number = SSID.count({username: usr, ssid: ssid, device: device}, function(err, c) {
-       console.log("DELETE angefordert für den User " +usr +" für die ID " +id);
-       if (c == 1) {
-          console.log(stat000);
+    var number = SSID.count({ssid: ssid}, function(err, c) {
+    if (c == 1) {
+            SSID.findOne({ssid:ssid}, function(err, e) {  
+            console.log(stat000);
+            console.log("Der User" +e.username+ " hat PUT angefordert");
 //check ob todo mit dere id vorhande für de user
-          var number2 = Todo.count({user: usr, _id: id}, function(err, d) {
-             if (d == 1) {
-                Todo.remove({_id:id, user:usr}, function(err) {
-                   console.log(stat003);
-                   console.log("");
-                   res.json(stat003);
-                   res.end();
-                });
-             }
-             else {
-                var number2 = Todo.count({sharedw: usr, _id: id}, function(err, f) {
-                   if (f == 1) {
-                      Todo.remove({_id:id, sharedw: usr}, function(err) {
-                         console.log(stat003);
-                         console.log("");
-                         res.json(stat003);
-                         res.end();
-                      });            
-                   }
-                   else {
-                      console.log(stat004);
-                      console.log("");
-                      res.json(stat004);
-                      res.end();
-                   }
-                });       
-             }
-          });
+            var number2 = Todo.count({user: e.username, _id: id}, function(err, d) {
+                if (d == 1) {
+                    Todo.remove({_id:id, user:e.username}, function(err) {
+                        console.log(stat003);
+                        console.log("");
+                        res.json(stat003);
+                        res.end();
+                    });
+                }
+                else {
+                    var number2 = Todo.count({sharedw: e.username, _id: id}, function(err, f) {
+                        if (f == 1) {
+                            Todo.remove({_id:id, sharedw: e.username}, function(err) {
+                               console.log(stat003);
+                               console.log("");
+                               res.json(stat003);
+                               res.end();
+                             });            
+                        }
+                        else {
+                            console.log(stat004);
+                            console.log("");
+                            res.json(stat004);
+                            res.end();
+                       }
+                    });       
+                }
+            });
+         });
        }                     
        else {
           console.log(stat001);
@@ -244,21 +250,21 @@ app.delete('/api/:Todo_id/:User_name/:User_ssid/:User_device', function(req, res
 
 //get (nach usr)
 app.get('/api', function(req, res) {
-    var usr = req.query.usr;
     var ssid = req.query.ssid;
-    var device = req.query.device;
-    var number = SSID.count({username: usr, ssid: ssid, device: device}, function(err, c) {
-       console.log("GET angefordert für den User " +usr);
+    var number = SSID.count({ssid: ssid}, function(err, c) {
        if (c == 1) {
-          console.log(stat000);
-          Todo.find({user: usr}, function (err, Todos) {
-             console.log("Got all the Todos for the user " +usr);
-             Todo.find({sharedw: usr}, function (err, Todos2) {
-                console.log("Got all the shared Todos for the user " +usr);
-                res.json(us.extend(Todos,Todos2));
-                res.end();
+          SSID.findOne({ssid:ssid}, function(err, e) {  
+             console.log(stat000);
+             console.log("Der User "+e.username+" hat GET angefordert");
+             Todo.find({user: e.username}, function (err, Todos) {
+                console.log("Got all the Todos for the user " +e.username);
+                Todo.find({sharedw: e.username}, function (err, Todos2) {
+                   console.log("Got all the shared Todos for the user " +e.username);
+                   res.json(us.extend(Todos,Todos2));
+                   res.end();
+                });
              });
-          });
+          });   
        }
        else {
           console.log(stat001);
